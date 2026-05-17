99d2de7810a08c9339ee6c4447c722260289076e
SOS Slovenija <sos@slovenija.si>
2026-05-17 18:11:52 +0200

    Initial: SOS Krizni Portal PWA

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'sos-kriza-v1';
+const urlsToCache = [
+  './resevalec.html',
+  './manifest.json',
+  './index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return cached version
+        }
+        return fetch(event.request); // Otherwise fetch from network
+      })
+  );
+});
